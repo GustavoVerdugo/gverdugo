2f154c9ccaabd474681b884f91580c0b755e9cb3
gustavoverdugo <maxyverdugo@gmail.com>
2020-10-30 12:28:44 -0300

    Updates

---
+++ precache-manifest.e7a3869d4d08a86360802c7a663708d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8dcb829f3262f538eb4cc2d6ea240df",
+    "url": "/gverdugo/index.html"
+  },
+  {
+    "revision": "76a3dfbfb70e43cabbf6",
+    "url": "/gverdugo/static/css/main.90a5cb83.chunk.css"
+  },
+  {
+    "revision": "cfa744a4f74d0f2eafc7",
+    "url": "/gverdugo/static/js/2.a9bdd820.chunk.js"
+  },
+  {
+    "revision": "76a3dfbfb70e43cabbf6",
+    "url": "/gverdugo/static/js/main.6278ca63.chunk.js"
+  },
+  {
+    "revision": "144c919502f9386facc5",
+    "url": "/gverdugo/static/js/runtime~main.05f881ef.js"
+  },
+  {
+    "revision": "88d3df885b8e1a3381c6987246deebfc",
+    "url": "/gverdugo/static/media/IMG_4141.88d3df88.jpg"
+  }
+]);
